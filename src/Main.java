e1e3049d3c7fcb0938100c18574df2861a84e3a9
Jairo <jairo.machicote@gmail.com>
2024-05-01 09:19:45 -0300

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        Scanner teclado = new Scanner(System.in);
+
+        int opcion;
+        do {
+            System.out.println("Opcion 1");
+            System.out.println("Opcion 2");
+            System.out.println("Opcion 3");
+            System.out.println("Opcion 4");
+            System.out.println("Opcion 5");
+            System.out.println("Opcion 6");
+            System.out.println("Opcion 7");
+            System.out.println("Seleccione opcion: ");
+            opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("a");
+                    break;
+                case 2:
+                    System.out.println("a");
+                    break;
+                case 3:
+                    System.out.println("a");
+                    break;
+                case 4:
+                    System.out.println("a");
+                    break;
+                case 5:
+                    System.out.println("a");
+                    break;
+                case 6:
+                    System.out.println("a");
+                    break;
+                case 7:
+                    break;
+            }
+        } while (opcion != 7);
 
     }
 }
